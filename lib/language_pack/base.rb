90980f6934eee97d5291c43e1e0c35c8be38206b
justinsaul <justinsaul@gmail.com>
2015-03-25 12:04:37 -0700

    adding vendor stack path to s3

--- lib/language_pack/base.rb
+++ lib/language_pack/base.rb
@@ -34,7 +34,7 @@ class LanguagePack::Base
       @id            = Digest::SHA1.hexdigest("#{Time.now.to_f}-#{rand(1000000)}")[0..10]
       @warnings      = []
       @deprecations  = []
-      @fetchers      = {:buildpack => LanguagePack::Fetcher.new(VENDOR_URL) }
+      @fetchers      = {:buildpack => LanguagePack::Fetcher.new(VENDOR_URL + "/" + @stack) }
 
       Dir.chdir build_path
     end
